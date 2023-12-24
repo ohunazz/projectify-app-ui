f3181b137aa602e2b3917c932bd73247a485d290
ohunazz <ohunazz@gmail.com>
2023-12-24 08:10:56 -0500

    Add SignIn

--- src/api/admin.js
+++ src/api/admin.js
@@ -20,4 +20,24 @@ class Admin {
             throw error;
         }
     }
+
+    async signIn(input) {
+        try {
+            const response = await fetch(`${this.url}/login`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(input)
+            });
+            if (!response.ok) {
+                const data = await response.json();
+                throw new Error(data.message);
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
 }
+
+export const admin = new Admin();
