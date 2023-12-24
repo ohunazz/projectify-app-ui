5b783b214a3361dca6f50c55ecffee04cc0cd4d4
ohunazz <ohunazz@gmail.com>
2023-12-24 07:44:59 -0500

    Add Sign Up

--- src/api/admin.js
+++ src/api/admin.js
@@ -2,4 +2,22 @@ class Admin {
     constructor() {
         this.url = `${process.env.REACT_APP_PROJECTIFY_API_URL}/admins`;
     }
+
+    async signUp(input) {
+        try {
+            const response = await fetch(`${this.url}/sign-up`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(input)
+            });
+            if (!response.ok) {
+                const data = await response.json();
+                throw new Error(data.message);
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
 }
