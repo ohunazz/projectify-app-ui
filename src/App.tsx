19b9ebe15c95f9c2b77556ac12af3a602a68e93d
ohunazz <ohunazz@gmail.com>
2024-01-07 10:23:07 -0500

    Finish Admin Style Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,5 @@
-import { ForgotPassword } from "./pages/admin-auth/forget-password/ForgetPassword";
-import { Login } from "./pages/admin-auth/login/Login";
-import { ResetPassword } from "./pages/admin-auth/reset-password/ResetPassword";
-import { Signup } from "./pages/admin-auth/sign-up/SignUp";
-
 const App = () => {
-    return (
-        <>
-            <ForgotPassword />
-            <ResetPassword />
-            <Login />
-            <Signup />
-        </>
-    );
+    return;
 };
 
 export { App };
