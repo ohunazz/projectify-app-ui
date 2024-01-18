5ff4e6200606f3dc47c29d1668f3c99d129dd919
ohunazz <ohunazz@gmail.com>
2024-01-18 18:56:12 -0500

    Create Private Route

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,38 +1,85 @@
 import {
     createBrowserRouter,
     createRoutesFromElements,
-    Route
+    Route,
 } from "react-router-dom";
-
 import { App } from "../App";
 import {
-    AdminSignup,
     AdminSignin,
-    AdminForgotPassword,
+    AdminSignup,
+    AdminForgetPassword,
     AdminResetPassword,
-    AdminPlatform
+    AdminPlatform,
+    AdminProjects,
+    AdminTasks,
+    TeamMemberSignin,
+    TeamMemberForgetPassword,
+    TeamMemberResetPassword,
+    TeamMemberCreatePassword,
+    TeamMemberPlatform,
+    TeamMemberPersonalTasks,
 } from "../pages";
+import { Private } from "./Private";
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
         <>
             <Route path="/" element={<App />} />
-            <Route path="/admin/sign-up" element={<AdminSignup />} />
-            <Route path="/admin/sign-in" element={<AdminSignin />} />
+            <Route path="admin/sign-up" element={<AdminSignup />} />
+            <Route path="admin/sign-in" element={<AdminSignin />} />
             <Route
-                path="/admin/forgot-password"
-                element={<AdminForgotPassword />}
+                path="admin/forget-password"
+                element={<AdminForgetPassword />}
             />
             <Route
-                path="/admin/reset-password"
+                path="admin/reset-password"
                 element={<AdminResetPassword />}
             />
+            <Route
+                path="admin/platform"
+                element={
+                    <Private
+                        component={<AdminPlatform />}
+                        userType="admin"
+                        to="../admin/sign-in"
+                    />
+                }
+            >
+                <Route path="projects" element={<AdminProjects />} />
+                <Route path="stories" element={<h1>Stories</h1>} />
+                <Route path="personal-tasks" element={<AdminTasks />} />
+                <Route path="team-members" element={<h1>Members</h1>} />
+            </Route>
 
-            <Route path="/platform" element={<AdminPlatform />}>
-                <Route path="projects" />
-                <Route path="stories" />
-                <Route path="personal-tasks" />
-                <Route path="team-members" />
+            <Route path="team-member/sign-in" element={<TeamMemberSignin />} />
+            <Route
+                path="team-member/forget-password"
+                element={<TeamMemberForgetPassword />}
+            />
+            <Route
+                path="team-member/reset-password"
+                element={<TeamMemberResetPassword />}
+            />
+            <Route
+                path="team-member/create-password"
+                element={<TeamMemberCreatePassword />}
+            />
+            <Route
+                path="team-member/platform"
+                element={
+                    <Private
+                        component={<TeamMemberPlatform />}
+                        userType="teamMember"
+                        to="../team-member/sign-in"
+                    />
+                }
+            >
+                <Route path="stories" element={<h1>Stories</h1>} />
+                <Route
+                    path="personal-tasks"
+                    element={<TeamMemberPersonalTasks />}
+                />
+                <Route path="team-members" element={<h1>Members</h1>} />
             </Route>
         </>
     )
