0f0baec46c632f5325e0f3e79cb9221d9941c6af
ohunazz <ohunazz@gmail.com>
2024-04-16 14:09:57 -0400

    Implement Admin Project actions and reducer

--- src/store/reducers/adminProjectReducer.ts
+++ src/store/reducers/adminProjectReducer.ts
@@ -1,13 +1,37 @@
 import { produce } from "immer";
 import { ProjectState } from "../state";
-import { ActionType, Actions, AddProjectAction } from "../actions";
+import {
+    ActionType,
+    Actions,
+    AddProjectAction,
+    AdminChangeProjectStatusAction,
+    AdminPopulateProjectContributorsAction,
+    AdminPopulateProjectsAction,
+    AdminUpdateProjectAction,
+    AdminUpdateProjectContributorStatus,
+    AdminUpdateProjectContributorsList
+} from "../actions";
+
+import {
+    AssignedContrubtorsState,
+    NotAssignedContributorsState,
+    ProjectContributor,
+    ProjectContributorBase
+} from "../../types";
 
 const adminProjectsReducer = produce(
     (draft: ProjectState, action: ActionType) => {
         switch (action.type) {
             case Actions.ADMIN_ADD_PROJECT: {
                 const payload = action.payload as AddProjectAction["payload"];
-                draft[payload.id] = { ...payload, numberOfContributors: 0 };
+                draft[payload.id] = {
+                    ...payload,
+                    numberOfContributors: 0,
+                    contributors: {
+                        assignedContributors: {},
+                        notAssignedContributors: {}
+                    }
+                };
                 return draft;
             }
 
