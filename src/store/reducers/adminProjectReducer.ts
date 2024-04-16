53afb20d2e3f1f514ef9cb4099bbfcaa9b9d55bc
ohunazz <ohunazz@gmail.com>
2024-04-16 14:17:19 -0400

    Update Admin Project  Reducer

--- src/store/reducers/adminProjectReducer.ts
+++ src/store/reducers/adminProjectReducer.ts
@@ -34,9 +34,109 @@ const adminProjectsReducer = produce(
                 };
                 return draft;
             }
+            case Actions.ADMIN_POPULATE_PROJECTS: {
+                const payload =
+                    action.payload as AdminPopulateProjectsAction["payload"];
+                return payload.reduce((acc: ProjectState, project) => {
+                    acc[project.id] = project;
+                    acc[project.id].contributors = {
+                        assignedContributors: {},
+                        notAssignedContributors: {}
+                    };
+                    return acc;
+                }, {});
+            }
 
-            default:
+            case Actions.ADMIN_CHANGE_PROJECT_STATUS: {
+                const payload =
+                    action.payload as AdminChangeProjectStatusAction["payload"];
+                const project = draft[payload.id];
+                if (project) {
+                    project.status = payload.status;
+                }
+
                 return draft;
+            }
+
+            case Actions.ADMIN_UPDATE_PROJECT: {
+                const payload =
+                    action.payload as AdminUpdateProjectAction["payload"];
+                draft[payload.id] = {
+                    ...draft[payload.id],
+                    ...payload.data
+                };
+                return draft;
+            }
+
+            case Actions.ADMIN_POPULATE_PROJECT_CONTRIBUTORS: {
+                const payload =
+                    action.payload as AdminPopulateProjectContributorsAction["payload"];
+                draft[payload.id].contributors = {
+                    assignedContributors:
+                        payload.data.assignedContributors.reduce(
+                            (
+                                acc: AssignedContrubtorsState,
+                                contributor: ProjectContributor
+                            ) => {
+                                acc[contributor.id] = contributor;
+                                return acc;
+                            },
+                            {}
+                        ),
+                    notAssignedContributors:
+                        payload.data.notAssignedContributors.reduce(
+                            (
+                                acc: NotAssignedContributorsState,
+                                contributor: ProjectContributorBase
+                            ) => {
+                                acc[contributor.id] = contributor;
+                                return acc;
+                            },
+                            {}
+                        )
+                };
+
+                return draft;
+            }
+
+            case Actions.ADMIN_UPDATE_PROJECT_CONTRIBUTOR_STATUS: {
+                const { id, status, teamMemberId } =
+                    action.payload as AdminUpdateProjectContributorStatus["payload"];
+                draft[id].contributors.assignedContributors[
+                    teamMemberId
+                ].status = status;
+                if (status === "ACTIVE") {
+                    draft[id].numberOfContributors += 1;
+                } else {
+                    draft[id].numberOfContributors -= 1;
+                }
+
+                return draft;
+            }
+
+            case Actions.ADMIN_UPDATE_PROJECT_CONTRIBUTORS_LIST: {
+                const payload =
+                    action.payload as AdminUpdateProjectContributorsList["payload"];
+                const { id, newContributors } = payload;
+
+                const assignedContributors =
+                    draft[id].contributors.assignedContributors;
+                const notAssignedContributors =
+                    draft[id].contributors.notAssignedContributors;
+
+                newContributors.forEach((contributor) => {
+                    assignedContributors[contributor.teamMemberId] = {
+                        joinedAt: contributor.joinedAt,
+                        status: contributor.status,
+                        ...notAssignedContributors[contributor.teamMemberId]
+                    };
+                    delete notAssignedContributors[contributor.teamMemberId];
+                });
+
+                draft[id].numberOfContributors += newContributors.length;
+
+                return draft;
+            }
         }
     }
 );
